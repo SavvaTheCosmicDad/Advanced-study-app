d4db35fdce7c28557a99b9a998b4976b1015f5dc
Savva <sevasteenko.savva@mail.ru>
2022-08-02 16:39:28 +0300

    Сделал модальные окна

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     const tabs = document.querySelectorAll(".tabheader__item"),
-    tabcontent = document.querySelectorAll(".tabcontent"),
-    tabparent = document.querySelector(".tabheader__items");
-        
+        tabcontent = document.querySelectorAll(".tabcontent"),
+        tabparent = document.querySelector(".tabheader__items");
+
     function hideTabContent() {
-        tabcontent.forEach((item) =>{
+        tabcontent.forEach((item) => {
             item.style.display = "none";
-        }); 
-        
+        });
+
         tabs.forEach(item => {
-        item.classList.remove("tabheader__item_active");
-        
-    });
+            item.classList.remove("tabheader__item_active");
+
+        });
     }
     hideTabContent();
 
@@ -24,10 +24,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
     tabparent.addEventListener("click", (event) => {
         const target = event.target;
-        
+
         if (target && target.classList.contains("tabheader__item")) {
             tabs.forEach((item, i) => {
-                if (target == item){
+                if (target == item) {
                     hideTabContent();
                     showTabContent(i);
                 }
@@ -37,19 +37,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Timer
 
-    const deadline ="2022-09-13";
+    const deadline = "2022-09-13";
 
     function getTimeRemaining(endtime) {
         const t = Date.parse(endtime) - Date.parse(new Date()),
             days = Math.floor(t / (1000 * 60 * 60 * 24)),
-            hours = Math.floor((t / (1000 * 60 *60)) % 24),
-            minutes = Math.floor((t / (1000 * 60 )) % 60),
-            seconds = Math.floor((t / 1000 ) % 60);
+            hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+            minutes = Math.floor((t / (1000 * 60)) % 60),
+            seconds = Math.floor((t / 1000) % 60);
 
         return {
             'total': t,
             'days': days,
-            'hours': hours, 
+            'hours': hours,
             'minutes': minutes,
             'seconds': seconds
         };
@@ -63,8 +63,8 @@ document.addEventListener("DOMContentLoaded", () => {
             seconds = timer.querySelector('#seconds'),
 
             timeInterval = setInterval(updateClock, 1000);
-        
-        function updateClock(){
+
+        function updateClock() {
             const t = getTimeRemaining(endtime);
 
             days.innerHTML = t.days;
@@ -72,11 +72,64 @@ document.addEventListener("DOMContentLoaded", () => {
             minutes.innerHTML = t.minutes;
             seconds.innerHTML = t.seconds;
 
-            if (t.total <= 0 ){
+            if (t.total <= 0) {
                 clearInterval(timeInterval);
             }
         }
     }
     setClock('.timer', deadline);
-    
+
+    //Modal
+
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+        modal = document.querySelector('.modal'),
+        modalClose = document.querySelector('[data-close]');
+
+        console.log(modalTrigger, modal, modalClose);
+
+    function openModal() {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+        //Если тригер уже был нажат, появление окна через таймер не будет
+        clearTimeout(modalTimerId);
+        //modal.classList.toggle('show');
+    }
+
+    function closeModal() {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow = '';
+        //modal.classList.toggle('show');
+    }
+
+    modalTrigger.forEach((item) => {
+        item.addEventListener('click', openModal);
+    });
+    modalClose.addEventListener('click', closeModal);
+
+    // Закрытие модалки при клике на подлжку
+    modal.addEventListener('click', (event) => {
+        if (event.target === modal) {
+            closeModal();
+        }
+    });
+    // Закрытие модалки при нажатии esc
+    document.addEventListener('keydown', (event) => {
+        if (event.code === 'Escape' && modal.classList.contains('show')) {
+            closeModal();
+        }
+    });
+
+    // Открытие окна через 3 секунды
+    const modalTimerId = setTimeout(openModal, 8000);
+
+    //Открытие модалки при скроле до конца страницы
+    function showModalByScroll() {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+    window.addEventListener('scroll', showModalByScroll);
 });
